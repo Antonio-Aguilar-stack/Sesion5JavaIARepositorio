932061634df204ddc949322df4c8fda60b510c49
Antonio-Aguilar-stack <luistopo3000@gmail.com>
2025-11-15 18:13:20 -0600

    version 1.2 cambio sanchez

--- Sesion5JavaIA/src/vista/GestionProductosPOO.java
+++ Sesion5JavaIA/src/vista/GestionProductosPOO.java
@@ -6,9 +6,15 @@ import java.util.Scanner;
 
 public class GestionProductosPOO {//inicia clase
     public static void main(String[] args) {//inicia main
-        Scanner entradaDato=new Scanner(System.in);
-        GestorProductos gestor =  new GestorProductos();
-        System.out.println("bienvenido Sistema de Gestion");
+        Scanner entradaDato=new Scanner(System.in);//captura cualquier tipo de dato
+        GestorProductos gestor =  new GestorProductos();//para uso y llamdo del metodo 
+       int ciclo;
+       
+       do{//inicio do
+           System.out.println("Menu Productos\n1. Agregra\n2. consultar\n3. Eliminar\n4. salir\n");
+           ciclo=entradaDato.nextInt();//para capturar opcion menu
+           switch (ciclo) {//inicio switch
+               case 1:
         System.out.println("introduce ID");
         int id=entradaDato.nextInt();
         System.out.println("Captura nombre:");
@@ -19,9 +25,12 @@ public class GestionProductosPOO {//inicia clase
         int stock=entradaDato.nextInt();
         
         gestor.agregarProducto(new Producto (id, nombre, precio, stock));//almacenar datos
-        
-        //consultar
-        gestor.listarProductos();
+                   break;
+               case 2:
+               gestor.listarProductos();
+                   break;
+               case 3:
+                           //consultar
         //busacrpor id
         System.out.println("Id a buscar:");
         Producto p = gestor.buscarPorId(entradaDato.nextInt());
@@ -30,6 +39,20 @@ public class GestionProductosPOO {//inicia clase
         gestor.eliminarProducto(entradaDato.nextInt());
         //consultar para actulizar
         gestor.listarProductos();
+                   break;
+               case 4:
+                   System.out.println("Finalizo programa O:");
+                   break;
+               case 5:break;
+               case 6:break;
+               default:System.out.println("Opcion invalida solo puede usar 1,2,3 y 4");
+                   break;
+           }//termina swicht
+       }while(ciclo!=4);
+       
+
+        
+
         
     }
     
